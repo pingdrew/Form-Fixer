285979038d1cce63a0cf7ab0eb5704fbbd2e5b35
pingdrew <pingdrew0@gmail.com>
2023-07-14 11:14:05 -0500

    basic JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,11 @@ var exerciseAPI = 'https://api.api-ninjas.com/v1/exercises?muscle=';
 var youtubeAPI = '';
 var searchButton = document.getElementById('search-button');
 var searchText = document.getElementById('search-box');
+var bullshit;
 
 searchButton.addEventListener('click', searched);
 
 function searched() {
   var input = searchText.value
-  
+
 }

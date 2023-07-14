f9674e4e0c702b4a78af953df0ba04f5cc457e5b
pingdrew <pingdrew0@gmail.com>
2023-07-14 10:50:51 -0500

    added basic starter js and script/link html tags

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+var exerciseAPI = 'https://api.api-ninjas.com/v1/exercises?muscle=';
+var youtubeAPI = '';
+var searchButton = document.getElementById('search-button');
+var searchText = document.getElementById('search-box');
+
+searchButton.addEventListener('click', searched);
+
+function searched() {
+  var input = searchText.value
+  
+}

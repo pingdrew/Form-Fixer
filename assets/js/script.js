2f9c067b3f43fbc8886770a76070bebcd92782da
JD <jondavidperson@gmail.com>
2023-07-17 17:37:53 -0500

    youtube id fetch working, added demonstration button that displays modal w matching YT vid

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,62 +1,6 @@
-var exerciseAPI = 'https://api.api-ninjas.com/v1/exercises?';
-var exerciseKey = 'bYqbwHUgCmk04IRweYUKJA==uYWz1DBHVfyCSUbZ';
-var youtubeAPI = 'https://www.googleapis.com/youtube/v3/search?';
-var youtubeKey = 'AIzaSyCp-bCzTWnxh6Vm1tC2h-HZmc5mrcaCuY0';
-
-
 var searchButton = document.getElementById('search-button');
 var muscleList = document.getElementById('muscle-groups');
 var fetchResults = document.getElementById('fetch-results');
-
-var newResult = document.createElement("li");
-newResult.setAttribute('class', 'box has-background-grey-lighter');
-
-searchButton.addEventListener('click', function (event) {
-  event.preventDefault();
-  var searchText = document.getElementById('search-box');
-
-  var search = searchText.value.trim();
-
-  generateNinjaResponse(search, '', '', '')
-});
-
-
-// TODO: Listen to Type list, fetch and display using a.value as "type="
-
-// TODO: Listen to Muscle list, fetch and display using a.value as "muscle="
-// muscleList.addEventListener('click', function (event) {
-//   var muscle = event.target.textContent;
-
-//   exerciseAPI = exerciseAPI + 'muscle=' + muscle + '&X-Api-Key' + exerciseKey;
-
-//   generateNinjaResponse()
-// });
-
-$('ul').children('li').on('click', function () {
-  var liElement =  $(this).closest('ul').attr('id');
-
-  if (liElement === 'exercise-type') {
-    var type = (this).innerText;
-    generateNinjaResponse('', type, '', '');
-
-  }else if (liElement === 'muscle-groups') {
-    var muscle = (this).innerText;
-    generateNinjaResponse('', '', muscle, '');
-
-  }else if (liElement === 'difficulty') {
-    var difficulty = (this).innerText;
-    generateNinjaResponse('', '', '', difficulty);
-  };
-});
-
-// TODO: Listen to Diff list, fetch and display using a.value as "difficulty="
-
-
-// fetchResults.addEventListener('click', function (event) {
-//   // TODO: Remove results other than event.target then make target full size with
-//   // description and iframe (or "See Demonstration" button to open modal)
-
-// });
 
 function generateNinjaResponse(search, type, muscle, difficulty) {
   var ajaxURL = 'https://api.api-ninjas.com/v1/exercises?'
@@ -96,10 +40,15 @@ function generateNinjaResponse(search, type, muscle, difficulty) {
         var innerText2 = document.createElement('p');
         innerText2.textContent = result[i].instructions;
 
+        var demoBtn = document.createElement('button');
+        demoBtn.setAttribute('class', 'button mt-2 has-background-grey-light demo-button');
+        demoBtn.textContent = 'Demonstration';
+        
         var resultList = document.getElementById('results-list')
         outerDiv.appendChild(innerTitle);
         outerDiv.appendChild(innerText1);
         outerDiv.appendChild(innerText2);
+        outerDiv.appendChild(demoBtn);
         resultList.appendChild(outerDiv);
       };
     },
@@ -108,3 +57,60 @@ function generateNinjaResponse(search, type, muscle, difficulty) {
     }
   });
 };
+
+function showVideo(id) {
+  var dim = document.querySelector('.dim');
+  var modal = document.querySelector('.modal');
+  var player = document.querySelector('iframe');
+
+  player.setAttribute('src', 'https://youtube.com/embed/' + id);
+
+  modal.style.display = 'block';
+  dim.style.display = 'block';
+
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = 'none';
+      dim.style.display = 'none';
+    }
+  }
+}
+
+searchButton.addEventListener('click', function (event) {
+  event.preventDefault();
+  var searchText = document.getElementById('search-box');
+
+  var search = searchText.value.trim();
+
+  generateNinjaResponse(search, '', '', '')
+});
+
+$('ul').children('li').on('click', function () {
+  var liElement =  $(this).closest('ul').attr('id');
+
+  if (liElement === 'exercise-type') {
+    var type = (this).innerText;
+    generateNinjaResponse('', type, '', '');
+
+  }else if (liElement === 'muscle-groups') {
+    var muscle = (this).innerText;
+    generateNinjaResponse('', '', muscle, '');
+
+  }else if (liElement === 'difficulty') {
+    var difficulty = (this).innerText;
+    generateNinjaResponse('', '', '', difficulty);
+  };
+});
+
+fetchResults.addEventListener('click', function (event) {
+  if (event.target.classList.contains('demo-button')) {
+    var search = 'Proper form for ' + $(event.target).parent().find('h2').text();
+
+    $.get('https://www.googleapis.com/youtube/v3/search?key=AIzaSyCp-bCzTWnxh6Vm1tC2h-HZmc5mrcaCuY0&part=snippet&type=video&maxResults=1&q='
+    + encodeURIComponent(search),
+      function (data) {
+        var id = data.items[0].id.videoId;
+        showVideo(id);
+      });
+  }
+})
